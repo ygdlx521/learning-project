add14f681b914b6e144b31092303b155ea0e7db2
yunlianghe <yunlianghe@appannie.com>
2019-10-16 11:00:11 +0800

    add Netty-learning

--- Netty-learning/src/main/java/online/daliang/netty/server/ServerHandler.java
+++ Netty-learning/src/main/java/online/daliang/netty/server/ServerHandler.java
@@ -8,11 +8,9 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
 public class ServerHandler extends ChannelInboundHandlerAdapter {
-    public static void main(String[] args) {
-        System.out.println("");
-    }
+
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf in = (ByteBuf) msg;
         System.out.println("NettyServer received: " + in.toString(CharsetUtil.UTF_8));
         ctx.write(in);
@@ -20,12 +18,12 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+    public void channelReadComplete(ChannelHandlerContext ctx)  {
         ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
     }
