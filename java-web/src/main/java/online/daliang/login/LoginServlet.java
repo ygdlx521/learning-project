2ad0d252e449535848c8fdac3c15ba483d4572ab
ygdlx521 <136658687@qq.com>
2019-11-17 22:43:27 +0800

    add login html, servlet, route

---
+++ java-web/src/main/java/online/daliang/login/LoginServlet.java
@@ -0,0 +1,35 @@
+package online.daliang.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created on 2019/11/17.
+ *
+ * @author daliang
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //
+        System.out.println("Received Login Requests!");
+        req.setCharacterEncoding("utf-8");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("login!!!登录啦");
+
+    }
+}
