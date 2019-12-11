7ab5a7c36aac1c3192f939bf5cdf15b18496b9f5
yunlianghe <yunlianghe@appannie.com>
2019-12-11 14:40:20 +0800

    learned spring mvc

--- spring-mvc/src/main/java/online/daliang/springmvc/controller/SpringMVCController.java
+++ spring-mvc/src/main/java/online/daliang/springmvc/controller/SpringMVCController.java
@@ -2,6 +2,7 @@ package online.daliang.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SpringMVCController {
@@ -9,6 +10,15 @@ public class SpringMVCController {
     @RequestMapping(value = "/hello")
     public String hello(){
         System.out.println("Hello SpringMVC!");
+        return "success";
+    }
+
+    @RequestMapping(value = "testRequestParam")
+    public String testRequestParam(
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "age") Integer age){
+        System.out.println(name);
+        System.out.println(age);
         return "success";
     }
 }
--- spring-mvc/src/main/webapp/index.jsp
+++ spring-mvc/src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
+<%@page language="java"  contentType="text/html; charset=utf-8" pageEncoding="UTF-8" %>
+
 <html>
 <body>
-<h2>Hello World!</h2>
+    <h2>Hello World!</h2>
+    <a href="/hello"> 测试请求</a>
+    <br>
+    <a href="testRequestParam?name=tom&age=22">测试请求参数</a>
+    <br>
+    <a href="testResponseParam?name=tom&age=22">测试请求参数</a>
 </body>
 </html>
