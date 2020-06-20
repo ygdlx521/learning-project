131b38a1d52d47a0c501d3791419232a72c0bcb8
yunlianghe <yunlianghe@appannie.com>
2020-06-20 17:25:46 +0800

    finished kafka mock producer

--- flink-learning/src/main/java/online/daliang/kafka/producer/KafkaLogProducer.java
+++ flink-learning/src/main/java/online/daliang/kafka/producer/KafkaLogProducer.java
@@ -13,7 +13,7 @@ public class KafkaLogProducer {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "master:9092,slave0:9092,slave1:9092");
+        props.put("bootstrap.servers", "node00:9092,node01:9092,node02:9092");
         props.put("acks", "-1");
         props.put("batch.size", 16384);
         props.put("linger.ms", 1);
@@ -22,31 +22,66 @@ public class KafkaLogProducer {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         KafkaProducer kafkaProducer = new KafkaProducer<String, String>(props);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 7000000; i++) {
             String jsonString;
-            Advertisement advertisement = AdvertisementLog.generateLog(String.valueOf(i));
-            jsonString = JSON.toJSONString(advertisement);
-            System.out.println(jsonString);
 
-            Member member = MemberLog.generateLog(String.valueOf(i));
-            jsonString = JSON.toJSONString(member);
-            System.out.println(jsonString);
+            if(i < 10) {
+                Advertisement advertisement = AdvertisementLog.generateLog(String.valueOf(i));
+                jsonString = JSON.toJSONString(advertisement);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_advertisement", jsonString)
+                );
+            }
 
-            MemberRegisterTypeLog.generateLog(String.valueOf(i), "webA");
-            jsonString = JSON.toJSONString(member);
-            System.out.println(jsonString);
+            if(i < 5000000) {
+                Member member = MemberLog.generateLog(String.valueOf(i));
+                jsonString = JSON.toJSONString(member);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_member", jsonString)
+                );
+            }
 
-            MemberTransaction memberTransaction = MemberTransactionLog.generateLog();
-            jsonString = JSON.toJSONString(memberTransaction);
-            System.out.println(jsonString);
+            if(i < 5000000) {
+                MemberRegisterType memberRegisterType = MemberRegisterTypeLog.generateLog(String.valueOf(i), "webA");
+                jsonString = JSON.toJSONString(memberRegisterType);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_member_register_type", jsonString)
+                );
+            }
 
-            MemberVipLevel memberVipLevel = MemberVipLevelLog.generateLog(String.valueOf(i));
-            jsonString = JSON.toJSONString(memberVipLevel);
-            System.out.println(jsonString);
+            if(i < 7000000) {
+                MemberTransaction memberTransaction = MemberTransactionLog.generateLog();
+                jsonString = JSON.toJSONString(memberTransaction);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_member_transaction", jsonString)
+                );
+            }
 
-            WebSite webSite = WebSiteLog.generateLog(String.valueOf(i), "webA");
-            jsonString = JSON.toJSONString(webSite);
-            System.out.println(jsonString);
+            if(i < 5) {
+                MemberVipLevel memberVipLevel = MemberVipLevelLog.generateLog(String.valueOf(i));
+                jsonString = JSON.toJSONString(memberVipLevel);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_member_vip_level", jsonString)
+                );
+            }
+
+            if(i < 5) {
+                WebSite webSiteA = WebSiteLog.generateLog(String.valueOf(i), "webA");
+                jsonString = JSON.toJSONString(webSiteA);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_website", jsonString)
+                );
+                WebSite webSiteB = WebSiteLog.generateLog(String.valueOf(i), "webB");
+                jsonString = JSON.toJSONString(webSiteB);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_website", jsonString)
+                );
+                WebSite webSiteC = WebSiteLog.generateLog(String.valueOf(i), "webC");
+                jsonString = JSON.toJSONString(webSiteC);
+                kafkaProducer.send(
+                        new ProducerRecord<String, String>("ods_website", jsonString)
+                );
+            }
 
 //            kafkaProducer.send(new ProducerRecord<String, String >("member_log", jsonString));
         }
---
+++ flink-learning/src/main/scala/online/daliang/streaming/ods/OdsEtlData.scala
@@ -0,0 +1,12 @@
+package online.daliang.streaming.ods
+
+object OdsEtlData {
+
+  val BOOTSTRAP_SERVERS = "bootstrap.servers"
+  val GROUP_ID = "group.id"
+  val RETRIES = "retries"
+  val TOPIC = "topic"
+
+
+
+}
