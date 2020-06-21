6db04f9ea914eaf42ed824324ff36b6f01a6afb0
yunlianghe <yunlianghe@appannie.com>
2020-06-21 18:35:35 +0800

    debug

--- flink-learning/src/main/scala/online/daliang/flink/streaming/ods/OdsEtlData.scala
+++ flink-learning/src/main/scala/online/daliang/flink/streaming/ods/OdsEtlData.scala
@@ -9,11 +9,13 @@ import online.daliang.flink.streaming.model.TopicAndValue
 import online.daliang.flink.streaming.sink.HbaseSink
 import online.daliang.flink.streaming.utils.ParseJsonData
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
+import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.common.time.Time
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
 import org.apache.flink.runtime.state.StateBackend
+import org.apache.flink.streaming.api.datastream.DataStreamSource
 import org.apache.flink.streaming.api.environment.{CheckpointConfig, StreamExecutionEnvironment}
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.streaming.api.scala.OutputTag
@@ -52,38 +54,62 @@ object OdsEtlData {
     env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.seconds(10)))
 
 
-    import scala.collection.JavaConverters._
-    val topicList = params.get(TOPIC).split(",").toBuffer.asJava
-    val consumerProps = new Properties()
-    consumerProps.setProperty(BOOTSTRAP_SERVERS, params.get(BOOTSTRAP_SERVERS))
-    consumerProps.setProperty(GROUP_ID, params.get(GROUP_ID))
-    val kafkaEventSource = new FlinkKafkaConsumer010[TopicAndValue](topicList, new TopicAndValueDeserializationSchema, consumerProps)
-    kafkaEventSource.setStartFromEarliest()
+    val properties = new Properties()
+    //    properties.setProperty("bootstrap.servers", "master:9092,slave0:9092,slave1:9092")
+    properties.setProperty("bootstrap.servers", "node00:9092,node01:9092,node02:9092")
+    properties.setProperty("group.id", "consumer-group")
+    properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("auto.offset.reset", "latest")
 
-    val dataStream = env.addSource(kafkaEventSource)
+    val stream: DataStreamSource[String] = env.addSource(
+      new FlinkKafkaConsumer010[String]("first_test", new SimpleStringSchema(), properties)
+    )
 
+    val producerProperties = new Properties()
+    producerProperties.setProperty("bootstrap.servers", "node00:9092,node01:9092,node02:9092")
+
+
+    stream.addSink(
+      new FlinkKafkaProducer010[String](
+        "second_test", new SimpleStringSchema(), producerProperties)
+    )
+
+    env.execute("Flink Read Write Kafka")
+
+
+//    import scala.collection.JavaConverters._
+//    val topicList = params.get(TOPIC).split(",").toBuffer.asJava
+//    val consumerProps = new Properties()
+//    consumerProps.setProperty(BOOTSTRAP_SERVERS, params.get(BOOTSTRAP_SERVERS))
+//    consumerProps.setProperty(GROUP_ID, params.get(GROUP_ID))
+//    val kafkaEventSource = new FlinkKafkaConsumer010[TopicAndValue](topicList, new TopicAndValueDeserializationSchema, consumerProps)
+//    kafkaEventSource.setStartFromEarliest()
 //
-//    val dataStream = env.addSource(kafkaEventSource).filter(item => {
-//      //先过滤非json数据
-//      val obj = ParseJsonData.getJsonData(item.value)
-//      obj.isInstanceOf[JSONObject]
+//    val dataStream = env.addSource(kafkaEventSource)
+//
+////
+////    val dataStream = env.addSource(kafkaEventSource).filter(item => {
+////      //先过滤非json数据
+////      val obj = ParseJsonData.getJsonData(item.value)
+////      obj.isInstanceOf[JSONObject]
+////    })
+//
+//    //将dataStream拆成两份 一份维度表写到hbase 另一份事实表数据写到第二层kafka
+//    val sideOutHbaseTag = new OutputTag[TopicAndValue]("hbaseSinkStream")
+//    val result = dataStream.process(new ProcessFunction[TopicAndValue, TopicAndValue] {
+//      override def processElement(value: TopicAndValue, ctx: ProcessFunction[TopicAndValue, TopicAndValue]#Context, out: Collector[TopicAndValue]): Unit = {
+//        value.topic match {
+//          case "ods_website" | "ods_advertisement" | "ods_member_vip_level" => ctx.output(sideOutHbaseTag, value)
+//          case _ => out.collect(value)
+//        }
+//      }
 //    })
-
-    //将dataStream拆成两份 一份维度表写到hbase 另一份事实表数据写到第二层kafka
-    val sideOutHbaseTag = new OutputTag[TopicAndValue]("hbaseSinkStream")
-    val result = dataStream.process(new ProcessFunction[TopicAndValue, TopicAndValue] {
-      override def processElement(value: TopicAndValue, ctx: ProcessFunction[TopicAndValue, TopicAndValue]#Context, out: Collector[TopicAndValue]): Unit = {
-        value.topic match {
-          case "ods_website" | "ods_advertisement" | "ods_member_vip_level" => ctx.output(sideOutHbaseTag, value)
-          case _ => out.collect(value)
-        }
-      }
-    })
-    // 侧输出流得到维度表数据，需要写入hbase的数据
-    //        result.getSideOutput(sideOutGreenPlumTag).addSink(new DwdGreenPlumSink)
-    result.getSideOutput(sideOutHbaseTag).addSink(new HbaseSink)
-    // 事实表数据写入第二层kafka
-    result.addSink(new FlinkKafkaProducer010[TopicAndValue](GlobalConfig.BOOTSTRAP_SERVERS, "", new DwdKafkaProducerSerializationSchema))
-    env.execute()
+//    // 侧输出流得到维度表数据，需要写入hbase的数据
+//    //        result.getSideOutput(sideOutGreenPlumTag).addSink(new DwdGreenPlumSink)
+//    result.getSideOutput(sideOutHbaseTag).addSink(new HbaseSink)
+//    // 事实表数据写入第二层kafka
+//    result.addSink(new FlinkKafkaProducer010[TopicAndValue](GlobalConfig.BOOTSTRAP_SERVERS, "", new DwdKafkaProducerSerializationSchema))
+//    env.execute()
   }
 }
